14156150e88595f3283a6b4abbd44056299f72db
kucherenkosveta <kucherenko55sss55@gmail.com>
2022-09-24 01:08:59 +0300

    3save

--- 1/1/main.cpp
+++ 1/1/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
+using namespace std;
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
     return 0;
 }
