479e2a186c13492fa4d27ab2e11dbba4b301212a
kucherenkosveta <kucherenko55sss55@gmail.com>
2022-09-24 01:05:37 +0300

    1save

---
+++ 1/1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by :) on 24.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
